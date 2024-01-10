2a05822bbab550def49dacf4dd0f2103253cc28e
Delta-fanboy <143608720+Delta-fanboy@users.noreply.github.com>
2024-01-10 11:48:23 +0100

    update!!!!!

---
+++ js/example.js
@@ -0,0 +1,23 @@
+let header = document.getElementById("header");
+header.innerText = "Hei";
+header.style.color = "purple";
+
+const temp = 23;
+
+let number1 = 30;
+let number2 = 20;
+let result;
+
+result = 100;
+result = 150;
+
+
+result = number1 + number2;
+text = "Resultatet blir: " + result;
+
+console.log(text);
+
+alert("hallo!")
+
+let name = promt("kva er ditt namn?")
+header.innerText = "velkommen, " + FirstName;
---
+++ js/ifelse.js
@@ -0,0 +1,18 @@
+let ageCar = 18;
+let ageScooter = 16;16
+
+let personAge = prompt("alder")
+
+if(personAge >= ageCar) {
+    console.log("Person1 er gammal nok til å kjøre bil.");
+}
+
+else if(personAge >= ageScooter){
+    console.log("Person1 er gammal nok til å kjøre moped.");
+    }
+    
+    else if{
+    console.log("du er ikkje gammal nok til å ta førar kort");
+    }
+
+    
